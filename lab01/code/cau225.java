8e377d7a7191f7922747e3807942968eb899466f
myanh5654 <thaimyanh2610@gmail.com>
2025-03-02 02:48:22 +0700

    ''

---
+++ lab01/code/cau225.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class cau225
+ {
+    public static void main(String[] args) {
+        // Nhập hai số từ người dùng
+        String strNum1 = JOptionPane.showInputDialog("Nhập số thứ nhất:");
+        String strNum2 = JOptionPane.showInputDialog("Nhập số thứ hai:");
+
+        // Chuyển đổi từ String sang double
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        // Thực hiện các phép toán
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        String quotient;
+
+        // Kiểm tra nếu số thứ hai là 0 để tránh lỗi chia cho 0
+        if (num2 != 0) {
+            quotient = String.valueOf(num1 / num2);
+        } else {
+            quotient = "Không thể chia cho 0!";
+        }
+
+        // Hiển thị kết quả
+        String message = "Tổng: " + sum +
+                         "\nHiệu: " + difference +
+                         "\nTích: " + product +
+                         "\nThương: " + quotient;
+
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
